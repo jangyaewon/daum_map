92cb6e52fe2bc7ae586c1c5def688614bb7d190f
Jang Yaewon <rainsimple@naver.com>
2018-07-14 14:06:31 +0000

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,20 @@
+class HomeController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to '/home/index'
+  end
+  
+  private
+  def post_params
+      params.require(:post).permit(:title, :lat, :lng)
+  end
+end
